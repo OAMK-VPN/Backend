bdf78eac3b3fd4cc8c6e0998bcd046e40e557c42
wangkui-din22sp <c3waku00@students.oamk.fi>
2023-11-27 17:09:37 +0200

    add users' signUp and login tests

--- src/test/java/backend/com/parcelsystem/ParcelsystemApplicationTests.java
+++ src/test/java/backend/com/parcelsystem/ParcelsystemApplicationTests.java
@@ -1,13 +1,45 @@
 package backend.com.parcelsystem;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ParcelsystemApplicationTests {
+import org.json.JSONException;
+import org.junit.Assert;
+import org.junit.Test;
 
-	@Test
-	void contextLoads() {
-	}
+import backend.com.parcelsystem.TestHttp;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+
+
+public class ParcelsystemApplicationTests {
+
+    @Test
+    public void usersRegisterAndLoginTest() throws JSONException, IOException {
+
+        FileWriter fileWriter = new FileWriter("SignUpJsonData.json");
+        fileWriter.write("");
+        fileWriter.flush();
+        fileWriter.close();
+
+        Assert.assertEquals(0, TestHttp.testUserSignUp(100)); 
+        Assert.assertEquals(0, TestHttp.testUserLogin()); 
+
+
+
+             
+        
+    }
+
+    public static void main(String[] args) throws JSONException, IOException {
+        ParcelsystemApplicationTests tests = new ParcelsystemApplicationTests();
+        tests.usersRegisterAndLoginTest();
+    }
 }
