465284cc5f50f6df9661d1425837233d54491efd
quan doan <ducquan190896@gmail.com>
2023-11-29 19:58:16 +0200

    fix the @JsonProperty for SendLockerCodeResponse

--- src/main/java/backend/com/parcelsystem/Models/Response/Locker/SendLockerCodeResponse.java
+++ src/main/java/backend/com/parcelsystem/Models/Response/Locker/SendLockerCodeResponse.java
@@ -1,11 +1,16 @@
 package backend.com.parcelsystem.Models.Response.Locker;
 
 import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Data
 public class SendLockerCodeResponse {
   private Long lockerId;
+  
+  @JsonProperty("isOpen")
   private boolean isOpen;
 }
