02a633fd13a0c89bec8a80e08c75851964dab414
wangkui-din22sp <c3waku00@students.oamk.fi>
2023-12-06 16:06:26 +0200

    cron job for generateParce at 3 am every day

--- src/main/java/backend/com/parcelsystem/Scheduletasks/ParcelDistribution.java
+++ src/main/java/backend/com/parcelsystem/Scheduletasks/ParcelDistribution.java
@@ -17,17 +17,17 @@ public class ParcelDistribution {
     @Autowired
     ParcelService parcelService;
 
-    @Scheduled(fixedRate = 2000)
+/*     @Scheduled(fixedRate = 2000)
     public void scheduleFixedRateTask() {
         System.out.println(
         "Fixed rate task - " + System.currentTimeMillis() / 1000);
-    }
+    } */
 
-    // @Scheduled(cron = "0 0 3 * * *") // Run at 3 am every day
-    // public void generateParcelsJob() {
-    //     parcelService.CheckAllSendExpiredParcels();
-    //     parcelService.CheckAllPickupExpiredParcels();
-    //     parcelService.assignAllParcelsToDrivers();
-    //     parcelService.generateParcelsAndSendToDriversByRobot();
-    // }
+    @Scheduled(cron = "0 0 3 * * *") // Run at 3 am every day
+    public void generateParcelsJob() {
+        parcelService.CheckAllSendExpiredParcels();
+        parcelService.CheckAllPickupExpiredParcels();
+        parcelService.assignAllParcelsToDrivers();
+        parcelService.generateParcelsAndSendToDriversByRobot();
+    }
 }
